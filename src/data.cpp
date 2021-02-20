f1ed3ef4b0ee1be488a8dc6f0261aa55371b7453
Henri Seppä <henri.seppa@tuni.fi>
2021-02-20 18:43:59 +0200

    added deletion for the http reply

--- src/data.cpp
+++ src/data.cpp
@@ -21,18 +21,21 @@ void Data::fetchDataFinGrid(const QString &url)
 
     QNetworkRequest req = QNetworkRequest(url);
 
-
+    //used to set the api key for requests
+    req.setRawHeader(QByteArray("x-api-key"),QByteArray("TVJel2EJEE3txr1XXtVt57X3H1Kyspyd3pHLvw4X"));
 
     QNetworkReply *reply = manager_->get(req);
 
+    //connect to error slot if error signaled
     connect(reply, &QNetworkReply::errorOccurred, this, &Data::error);
 
 }
 
 void Data::downloadCompleted(QNetworkReply *reply)
 {
-    //after the download, reade the data
+    //after the download, read the data
     qDebug() << reply->readAll();
+    reply->deleteLater();
 }
 
 void Data::error(QNetworkReply::NetworkError error)
@@ -40,4 +43,5 @@ void Data::error(QNetworkReply::NetworkError error)
     Q_UNUSED(error);
     auto *reply = qobject_cast<QNetworkReply *>(sender());
     qDebug() << reply->readAll();
+    reply->deleteLater();
 }
