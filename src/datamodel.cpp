b966d6ccc16c84acbe8171553b005f2121c1fa97
Henri Seppä <henri.seppa@tuni.fi>
2021-03-17 17:30:38 +0200

    added getaverage method

--- src/datamodel.cpp
+++ src/datamodel.cpp
@@ -4,3 +4,21 @@ DataModel::DataModel(QObject *parent) : QObject(parent)
 {
 
 }
+
+std::vector<std::shared_ptr<Data> > DataModel::getAllData()
+{
+    return dataVector;
+}
+
+qreal DataModel::getAverage(QString id)
+{
+    auto result = data.find(id);
+
+    if (result == data.end()){
+        return 0;
+    }
+
+    return 1;
+}
+
+
--- src/datamodel.hh
+++ src/datamodel.hh
@@ -3,6 +3,7 @@
 
 #include <QObject>
 #include <memory>
+#include <vector>
 #include "data.hh"
 
 class DataModel : public QObject
@@ -11,8 +12,14 @@ class DataModel : public QObject
 public:
     explicit DataModel(QObject *parent = nullptr);
     Data getDataByID(QString id);
+
+    std::vector<std::shared_ptr<Data>> getAllData();
+
+    qreal getAverage(QString id);
+
 private:
     std::map<QString,std::shared_ptr<Data>> data;
+    std::vector<std::shared_ptr<Data>> dataVector;
     std::vector<std::pair<QDateTime,qreal>> calculateMonthlyAvg(QString id);
     std::vector<std::pair<QDateTime,qreal>> calculateMonthlyMinMaxAvg(QString id);
 
